f4c8871a3b08ac966b613f190866d31e200e9b8f
s1nn3rv2 <62899437+s1nn3rv2@users.noreply.github.com>
2026-03-01 19:24:00 +0100

    feat: use webkit_disable_dmabuf_renderer=1 by default on wayland

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    koda_lib::run()
+    #[cfg(target_os = "linux")]
+    {
+        use std::env;
+        if env::var("XDG_SESSION_TYPE").unwrap_or_default() == "wayland" {
+            unsafe {
+                env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+            }
+        }
+    }
+
+    koda_lib::run();
 }
